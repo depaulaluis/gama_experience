5b0f1aca987edb92a5a459e9c598795c7f09e514
Luis Henrique <henriquesilvadepaula@gmail.com>
2019-09-06 01:16:22 -0300

    Aprendendo react

---
+++ src/Entrada.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Entrada extends Component {
+    render(){
+        return(
+        <span>Entrada</span>
+        <input type="text" onChange-(this.pro) />
+        <button>Procurar</button>
+
+        )
+    }
+}
+
+export default Entrada;
---
+++ src/Usuario.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Entrada extends Component {
+    render(){
+        return(
+        <img scr="" alt="avatar" />
+        <ul>
+            <li>Usuário: depaulaluis</li>
+            <li>Nome: Luis Henrique</li>
+            <li>Blog: blog</li>
+        </ul>
+
+        )
+    }
+}
+
+export default Usuario;
